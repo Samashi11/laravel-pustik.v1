3dedf93cc38defafc744c6cec8f633f4d880479f
nurilanwaralabsory <nurilanwaralabsory@gmail.com>
2024-02-21 13:58:53 +0700

    Membuat project baru, installasi laravel breeze, dan set up database migration

---
+++ database/migrations/2024_02_21_062036_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('author', 200);
+            $table->string('publisher', 200);
+            $table->string('isbn', 13)->unique();
+            $table->string('cover');
+            $table->enum('status', ['available', 'unavailable'])->default('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ database/migrations/2024_02_21_062103_create_borrowers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('borrowers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('phone_number', 20);
+            $table->text('address');
+            $table->unsignedBigInteger('book_id');
+            $table->foreign('book_id')->references('id')->on('books');
+            $table->date('borrow_date');
+            $table->date('return_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('borrowers');
+    }
+};
